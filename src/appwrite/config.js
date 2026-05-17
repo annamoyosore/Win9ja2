06e72ce331c71bfd7cf6ed64e2af4cfcb2477b0e
annamoyosore <annamoyosore@gmail.com>
2026-05-17 15:57:32 +0100

    Update config.js

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -1,11 +1,21 @@
 import { Client, Account, Databases, ID, Query } from "appwrite";
 
+// =====================
+// INIT APPWRITE CLIENT
+// =====================
 const client = new Client();
 
 client
   .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
   .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
 
+// =====================
+// SERVICES
+// =====================
 export const account = new Account(client);
 export const databases = new Databases(client);
-export { ID, Query };
+
+// =====================
+// HELPERS
+// =====================
+export { client, ID, Query };
