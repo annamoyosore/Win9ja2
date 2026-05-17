150c2af530355918f44800862db2e683d922c6c5
annamoyosore <annamoyosore@gmail.com>
2026-05-17 15:46:31 +0100

    Update pages

---
+++ src/pages/Trade.jsx
@@ -0,0 +1,14 @@
+import Chart from "../components/Chart";
+import TradePanel from "../components/TradePanel";
+
+export default function Trade() {
+  return (
+    <div>
+      <h2>Live Trading</h2>
+
+      <Chart />
+
+      <TradePanel />
+    </div>
+  );
+}
