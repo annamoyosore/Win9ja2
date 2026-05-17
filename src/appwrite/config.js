f0127d55c15d8c00f3635ddb9b0f5610da80f9ad
annamoyosore <annamoyosore@gmail.com>
2026-05-17 15:30:42 +0100

    Update config.js

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -1,24 +1,11 @@
-import { Client, Account, Databases } from "appwrite";
+import { Client, Account, Databases, ID, Query } from "appwrite";
 
-export const CONFIG = {
-  endpoint: "https://cloud.appwrite.io/v1",
-  projectId: "YOUR_PROJECT_ID",
-  databaseId: "YOUR_DB_ID",
+const client = new Client();
 
-  collections: {
-    users: "users",
-    wallets: "wallets",
-    lobby: "lobby",
-    matches: "matches",
-    transactions: "transactions"
-  },
-
-  adminIds: ["ADMIN_ID1", "ADMIN_ID2"]
-};
-
-const client = new Client()
-  .setEndpoint(CONFIG.endpoint)
-  .setProject(CONFIG.projectId);
+client
+  .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
+  .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
 
 export const account = new Account(client);
-export const db = new Databases(client);
+export const databases = new Databases(client);
+export { ID, Query };
