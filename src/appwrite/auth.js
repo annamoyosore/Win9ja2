258f9efa36c5801a243538e445e7928654376a75
annamoyosore <annamoyosore@gmail.com>
2026-05-17 15:31:39 +0100

    Update auth.js

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -1,9 +1,21 @@
-import { account } from "./config.js";
+import { account, ID } from "./config";
 
-export const login = (email, password) =>
-  account.createEmailSession(email, password);
+// REGISTER
+export const registerUser = (email, password, name) => {
+  return account.create(ID.unique(), email, password, name);
+};
 
-export const register = (email, password, name) =>
-  account.create("unique()", email, password, name);
+// LOGIN
+export const loginUser = (email, password) => {
+  return account.createEmailPasswordSession(email, password);
+};
 
-export const getUser = () => account.get();
+// GET USER
+export const getUser = () => {
+  return account.get();
+};
+
+// LOGOUT
+export const logoutUser = () => {
+  return account.deleteSession("current");
+};
